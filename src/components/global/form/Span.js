5974538429f967752e69802d80b74d904be37b79
Toby Keech <Toby.Keech@embark.co.uk>
2024-02-01 16:16:19 +0000

    added span componenent

--- src/components/global/form/Span.js
+++ src/components/global/form/Span.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 const Span = styled.span`
     margin: ${({ margin }) => margin};
     font-size: ${({ fsize }) => fsize}em;
-    color: ${(props) => props.theme.text};
+    color: ${(props) => props.theme.label};
 
     @media all and (max-width: ${({ theme }) => theme.layout.xsWidth}) {
         ${props =>
